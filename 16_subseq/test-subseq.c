7a94358fc948212be5eb61f23b45ef5adef170e5
HuseynHajiyev <hhajiyev@hotmail.com>
2019-09-28 16:17:54 +0400

    Modified maxSeq.c

--- 16_subseq/test-subseq.c
+++ 16_subseq/test-subseq.c
@@ -6,7 +6,7 @@ size_t maxSeq(int *array, size_t n);
 
 int main(void) 
 {
-  int array1[] = {1, 2, 3, 2};
+  int array1[] = {1, 2, 3, 4, 5, 6};
   int array2[] = {2, -3, 5, 6, 8};
   int array3[] = {5};
   int array4[] = {2, 4, 3, 6, 10, 15, -1, 7, 8, 2};
@@ -14,7 +14,7 @@ int main(void)
   int array6[] = {2,2,2,3};
 
 
-  printf("array1 = %ld\n", maxSeq(array1, 4));
+  printf("array1 = %ld\n", maxSeq(array1, 6));
   printf("array2 = %ld\n", maxSeq(array2, 5));
   printf("array3 = %ld\n", maxSeq(array3, 1));
   printf("array4 = %ld\n", maxSeq(array4, 10));
@@ -26,9 +26,9 @@ int main(void)
     printf("Failed on NULL\n");
     exit(EXIT_FAILURE);
   }
-  if (maxSeq(array1, 4) != 3) 
+  if (maxSeq(array1, 6) != 6) 
   {
-    printf("Failed on 1,2,3,2\n");
+    printf("Failed on 1,2,3,4,5,6\n");
     exit(EXIT_FAILURE);
   }
   if (maxSeq(array2, 5) != 4) 
