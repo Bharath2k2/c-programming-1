56df7743a89fb4d3bf36940a12b6f654769943d7
HuseynHajiyev <hhajiyev@hotmail.com>
2019-10-13 21:15:50 +0400

    Did assignment

--- c3prj1_deck/deck.c
+++ c3prj1_deck/deck.c
@@ -7,18 +7,18 @@ void assert_card_valid(card_t c);
 
 void print_hand(deck_t * hand)
 {
-  for(size_t i = 0; i < *(*hand).n_cards; ++i)
+  for(size_t i = 0; i < hand->n_cards; ++i)
   {
-    print_card(*(*hand).cards[i]);
+    print_card(hand->cards[i]);
     printf(" ");
   }
 }
 
 int deck_contains(deck_t * d, card_t c) 
 {
-  for(size_t i = 0; i < (*d).n_cards; ++i)
+  for(size_t i = 0; i < d->n_cards; ++i)
   {
-    if(c.value == (*(*d).cards[i]).value && c.suit == (*(*d).cards[i]).suit)
+    if(c.value == d->cards[i]->value && c.suit == d->cards[i]->suit)
     {
       return 1;
     }
@@ -32,25 +32,25 @@ void shuffle(deck_t * d)
   for(size_t i = (*d).n_cards-1; i >= 1 ; --i)
   {
     size_t randomIndex = random()%i;
-    card_t **temp = (*d).cards[i];
-    (*d).cards[i] = (*d).cards[randomIndex];
-    (*d).cards[randomIndex] = temp;
+    card_t **temp = d->cards[i];
+    d->cards[i] = d->cards[randomIndex];
+    d->cards[randomIndex] = temp;
   }
 }
 
 void assert_full_deck(deck_t * d) 
 {
-  size_t numberOfComparisons = (*d).n_cards-1;
+  size_t numberOfComparisons = (d->n_cards)-1;
   for(size_t i = 0; i < numberOfComparisons; ++i)
   {
     if(i != numberOfComparisons-1){
-      assert_card_valid(*(*d).cards[i]);
+      assert_card_valid(d->cards[i]));
     } else {
-      assert_card_valid(*(*d).cards[i+1]);
+      assert_card_valid(d->cards[i+1]);
     }
     for(size_t j = i + 1; j <= numberOfComparisons; ++j)
     {
-        assert((*(*d).cards[i].value != *(*d).cards[j].value)&&(*(*d).cads[i].suit != *(*d).cads[j].suit));
+        assert((d->cards[i]->value != d->cards[j]->value)&&(d->cards[i]->suit != d->cards[j]->suit));
     }
   }
 }
