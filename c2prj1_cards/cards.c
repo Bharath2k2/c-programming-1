b29132d8b0f0949b6cc5b3f72342c99701923ea6
Coursera Learner <nobody@nowhere.nul>
2019-10-19 05:05:22 +0000

    Fixed cards.c file

--- c2prj1_cards/cards.c
+++ c2prj1_cards/cards.c
@@ -4,16 +4,16 @@
 #include "cards.h"
 
 
-void assert_card_valid(card_t c) 
+void assert_card_valid(card_t c)
 {
   assert((c.value >= 2 && c.value <= VALUE_ACE) && (c.suit >= SPADES && c.suit < NUM_SUITS));
 
 }
 
-const char* ranking_to_string(hand_ranking_t r) 
+const char* ranking_to_string(hand_ranking_t r)
 {
   switch(r)
-  {
+    {
     case STRAIGHT_FLUSH:
       return "STRAIGHT_FLUSH";
     case FOUR_OF_A_KIND:
@@ -32,39 +32,39 @@ const char* ranking_to_string(hand_ranking_t r)
       return "PAIR";
     case NOTHING:
       return "NOTHING";
-  }
+    }
   return "";
 }
 
-char value_letter(card_t c) 
+char value_letter(card_t c)
 {
- if(c.value >= 2 && c.value <= 9)
- {
-   return '0' + c.value;
- }
- switch (c.value)
- {
-  case 10:
-    return '0';
-  case VALUE_JACK:
-    return 'J';
-  case VALUE_QUEEN:
-    return 'Q';
-  case VALUE_KING:
-    return 'K';
-  case VALUE_ACE:
-    return 'A';
-  default:
-    return EXIT_FAILURE;
-  }
+  if(c.value >= 2 && c.value <= 9)
+    {
+      return '0' + c.value;
+    }
+  switch (c.value)
+    {
+    case 10:
+      return '0';
+    case VALUE_JACK:
+      return 'J';
+    case VALUE_QUEEN:
+      return 'Q';
+    case VALUE_KING:
+      return 'K';
+    case VALUE_ACE:
+      return 'A';
+    default:
+      return EXIT_FAILURE;
+    }
 
 }
 
 
-char suit_letter(card_t c) 
+char suit_letter(card_t c)
 {
   switch (c.suit)
-  {
+    {
     case SPADES:
       return 's';
     case HEARTS:
@@ -72,48 +72,48 @@ char suit_letter(card_t c)
     case DIAMONDS:
       return 'd';
     case CLUBS:
-      return 'c';   
+      return 'c';
     default:
       return EXIT_FAILURE;
-  }
+    }
 }
 
-void print_card(card_t c) 
+void print_card(card_t c)
 {
-  printf("%c%c", value_letter(c), suit_letter(c));
+  printf("%c%c ", value_letter(c), suit_letter(c));
 }
 
-card_t card_from_letters(char value_let, char suit_let) 
+card_t card_from_letters(char value_let, char suit_let)
 {
   card_t temp;
 
   if(value_let >= '2' && value_let <= '9')
-  {
-    temp.value = value_let - '0';
-  } else 
-  {
-      switch (value_let)
     {
-      case '0':
-        temp.value = 10;
-        break;
-      case 'J':
-        temp.value = VALUE_JACK;
-        break;
-      case 'Q':
-        temp.value = VALUE_QUEEN;
-        break;
-      case 'K':
-        temp.value = VALUE_KING;
-        break;
-      case 'A':
-        temp.value = VALUE_ACE; 
-        break;
+      temp.value = value_let - '0';
+    } else
+    {
+      switch (value_let)
+	{
+	case '0':
+	  temp.value = 10;
+	  break;
+	case 'J':
+	  temp.value = VALUE_JACK;
+	  break;
+	case 'Q':
+	  temp.value = VALUE_QUEEN;
+	  break;
+	case 'K':
+	  temp.value = VALUE_KING;
+	  break;
+	case 'A':
+	  temp.value = VALUE_ACE;
+	  break;
+	}
     }
-  }
 
   switch(suit_let)
-  {
+    {
     case 's':
       temp.suit = SPADES;
       break;
@@ -126,15 +126,15 @@ card_t card_from_letters(char value_let, char suit_let)
     case 'c':
       temp.suit = CLUBS;
       break;
-  }
+    }
 
   assert_card_valid(temp);
 
   return temp;
 }
 
-card_t card_from_num(unsigned c) 
-{ 
+card_t card_from_num(unsigned c)
+{
   int modValue;
   int modSuit;
   card_t temp;
@@ -143,7 +143,7 @@ card_t card_from_num(unsigned c)
   modSuit =c%4;
 
   switch(modValue)
-  {
+    {
     case 0:
       temp.value = VALUE_KING;
       break;
@@ -153,10 +153,10 @@ card_t card_from_num(unsigned c)
     default:
       temp.value = modValue;
       break;
-  }
+    }
 
   switch(modSuit)
-  {
+    {
     case 0:
       temp.suit = SPADES;
       break;
@@ -169,8 +169,8 @@ card_t card_from_num(unsigned c)
     case 3:
       temp.suit = CLUBS;
       break;
-  }
-  
+    }
+
   assert_card_valid(temp);
 
   return temp;
