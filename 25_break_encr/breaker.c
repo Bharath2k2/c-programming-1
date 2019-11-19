8f910baed057fc7760a7c1446dc1c5e6552aa85b
Coursera Learner <nobody@nowhere.nul>
2019-11-19 12:20:23 +0000

    Did it

--- 25_break_encr/breaker.c
+++ 25_break_encr/breaker.c
@@ -6,47 +6,47 @@
 
 int decypherKey(FILE * stream, int * arr)
 {
-    int ch;
-    while((ch = fgetc(stream)) != EOF)
+  int ch;
+  while((ch = fgetc(stream)) != EOF)
     {
-        if(isalpha(ch))
-        {
-            ++arr[tolower(ch) - 'a'];
-        }
+      if(isalpha(ch))
+	{
+	  ++arr[tolower(ch) - 'a'];
+	}
     }
-    int index = 0;
-    int max = 0;
-    for(int i = 0; i < 25; ++i)
+  int index = 0;
+  int max = 0;
+  for(int i = 0; i < 25; ++i)
     {
-        if(arr[i] > max)
-        {
-            index = i;
-            max = arr[i];
-        }
+      if(arr[i] > max)
+	{
+	  index = i;
+	  max = arr[i];
+	}
     }
-    int count = 0;
-    int key;
-    if (index >= ('e' - 'a')) key=index-('e'-'a') ;
-    else key=(26-('e'-'a'))+ index; 
-    return key;
+  int count = 0;
+  int key;
+  if (index >= ('e' - 'a')) key=index-('e'-'a') ;
+  else key=(26-('e'-'a'))+ index;
+  return key;
 }
 int main(int argc, char ** argv)
 {
-    int alphArr[26] = {0};
-    if(argc != 2)
+  int alphArr[26] = {0};
+  if(argc != 2)
     {
-        fprintf(stderr,"Wrong Input format: Include 1 file name\n");
-        return EXIT_FAILURE;
+      fprintf(stderr,"Wrong Input format: Include 1 file name\n");
+      return EXIT_FAILURE;
     }
-    FILE * encrypFile = fopen(argv[1],"r");
-    if(encrypFile == NULL)
+  FILE * encrypFile = fopen(argv[1],"r");
+  if(encrypFile == NULL)
     {
-        perror("Failed to open file");
+      perror("Failed to open file");
     }
-    int key = decypherKey(encrypFile, alphArr);
-    printf("%d\n", key);
-    if(fclose(encrypFile) != 0)
+  int key = decypherKey(encrypFile, alphArr);
+  printf("%d\n", key);
+  if(fclose(encrypFile) != 0)
     {
-        perror("failed to close file");
-    }  
+      perror("failed to close file");
+    }
 }
