4dbb99a204a9ddf9365c3dd0e6c175ce6a28be33
HuseynHajiyev <hhajiyev@hotmail.com>
2019-12-23 23:54:46 +0400

    First submission

--- 29_outname/outname.c
+++ 29_outname/outname.c
@@ -3,6 +3,10 @@
 #include <string.h>
 #include "outname.h"
 
-char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+char * computeOutputFileName(const char * inputName) 
+{
+  char * outputName = malloc((strlen(inputName)+11)*sizeof(*inputName));
+  strcpy(outputName, inputName);
+  strcat(outputName, ".counts");
+  return outputName;
 }
