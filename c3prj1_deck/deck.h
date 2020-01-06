a7ff2fe3cda66e394e9238d83896557362aff164
HuseynHajiyev <hhajiyev@hotmail.com>
2020-01-06 22:20:34 +0400

    Y

--- c3prj1_deck/deck.h
+++ c3prj1_deck/deck.h
@@ -2,21 +2,17 @@
 #define DECK_H
 #include <stdlib.h>
 #include "cards.h"
+#include "cards.c"
 
 struct deck_tag {
 	  card_t ** cards;
 	    size_t n_cards;
-};
+}
 typedef struct deck_tag deck_t;
 
+card_t card_from_num(unsigned c);
 void print_hand(deck_t * hand);
 int deck_contains(deck_t * d, card_t c) ;
 void shuffle(deck_t * d);
 void assert_full_deck(deck_t * d) ;
-//The below functions will be done in course 4.
-deck_t * make_deck_exclude(deck_t * excluded_cards);
-void add_card_to(deck_t * deck, card_t c);
-card_t * add_empty_card(deck_t * deck);
-void free_deck(deck_t * deck) ;
-deck_t * build_remaining_deck(deck_t ** hands, size_t n_hands) ;
 #endif
