74192875e3b4ccbf265a6a4e26cdbce769bf5514
HuseynHajiyev <hhajiyev@hotmail.com>
2020-01-22 19:59:28 +0400

    y

--- c4prj3_finish/main.c
+++ c4prj3_finish/main.c
@@ -49,20 +49,21 @@ int main(int argc, char ** argv) {
     fprintf(stderr, "not valid  input file\n");
     return EXIT_FAILURE ;
   }
-  deck_t ** deck_array=NULL;
+  deck_t ** deck_array = NULL;
   size_t n_hands =0 ;
   future_cards_t * fc=malloc(sizeof(*fc));
   fc->decks=NULL;
   fc->n_decks=0;
   deck_array=read_input(f,&n_hands,fc);
   //Create a deck with the remaining cards
-  deck_t* sh=build_remaining_deck(deck_array, n_hands);
-  int win_array[n_hands+1];
-   for(int u=0;u<n_hands+1;u++) win_array[u]=0;
+  deck_t* sh = build_remaining_deck(deck_array, n_hands);
+  int * win_array = malloc((n_hands + 1) * sizeof(*win_array));
+   for(int u=0; u < n_hands + 1; ++u) win_array[u]=0;
   // Do each Monte Carlo trial (repeat num_trials times)
   int num_trials = 10000;
   if (argc ==3) num_trials=atoi(argv[2]);
-  for(int i=0;i<num_trials;i++){
+  for(int i=0; i < num_trials; ++i)
+  {
     //Shuffle the deck of remaining cards
     shuffle(sh);
     //Assign unknown cards from the shuffled deck
@@ -72,18 +73,22 @@ int main(int argc, char ** argv) {
     win_array[c]++;
   }
   //you just need to print your results
-  for(size_t j=0;j<n_hands;j++){
+  for(size_t j=0;j < n_hands; ++j)
+  {
     printf("Hand %zu won %u / %u times (%.2f%%)\n",j,win_array[j],num_trials,(((float)win_array[j])/num_trials)*100);
   }
   printf( "And there were %u ties\n",win_array[n_hands]);
   //free any memory you allocated
-  for(int x=0;x<n_hands;x++){
+  for(int x = 0; x < n_hands; ++x)
+  {
     free_deck(deck_array[x]);
   }
   free(deck_array);
-  for(int o=fc->n_decks-1 ;o>=0;o--){
+  for(int o = fc->n_decks - 1; o >= 0; --o)
+  {
     if(fc->decks[o].n_cards != 0) free(fc->decks[o].cards);
   }
+  free(win_array);
   free(fc->decks);
   free(fc);
   free_deck(sh);
